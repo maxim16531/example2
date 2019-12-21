72c79dfb7cff066fbfcbefb81bfa723c2487ea61
maxim16531 <maximkoroteev2016@yandex.ru>
2019-12-21 19:49:09 +0300

    commit

--- test2.js
+++ test2.js
@@ -6,18 +6,25 @@ class Ship{
            this.status=false;          
     }
     start(){
+        this.status=true;
        console.log("Корабль отправился");
     }
     stop(){
+        this.status=false;
         console.log("Корабль остановился");
     }
-    move(){
-        console.log("Корабль " + this.name + " имеет широту " + this.latitude + " и долготу " + this.longitude + " затем Корабль "+ this.name + " приплыл на широту 66° и долготу 77° ");
-        
+    isInTheWay(){
+        return status
     }
-
+    move(latitude,longitude){    
+        this.latitude=latitude;
+        this.longitude=longitude;
+        console.log("Корабль " + this.name + " имеет широту " + this.latitude + " и долготу " + this.longitude + " затем Корабль "+ this.name + " приплыл на широту 66 и долготу 77");  
+    } 
 };
-var s=new Ship("Аврора","55°","44°","66°","77°");
+var s=new Ship("Аврора", 66 ,77);
 s.start();
-s.move();
+s.move(55,44);
 s.stop();
+
+
